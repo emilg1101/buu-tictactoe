0c5aeae9b4bf031669a5da87bb948b42ccf431ce
Nazar <nazarfatichov@mail.ru>
2019-06-09 16:38:59 +0300

    New game view

--- console/ConsoleDisplay.cpp
+++ console/ConsoleDisplay.cpp
@@ -1,19 +1,97 @@
 #include <iostream>
+#include <windows.h>
 #include "ConsoleDisplay.h"
 
 using namespace std;
+
+int currentCoordX;
+int currentCoordY;
+
+CONSOLE_SCREEN_BUFFER_INFO csbi;
+HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+COORD destCoord;
 
 ConsoleDisplay::ConsoleDisplay() {}
 
 ConsoleDisplay::~ConsoleDisplay() {}
 
 Configuration ConsoleDisplay::getConfiguration() {
+
     Configuration configuration = Configuration();
     configuration.IS_MULTIPLAYER = true;
     return configuration;
 }
 
+void moveToMessageCoord(){
+    destCoord.X = 0;
+    destCoord.Y = 12;
+    SetConsoleCursorPosition(hStdout, destCoord);
+}
+
+void saveCurrentCoord(){
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if(GetConsoleScreenBufferInfo (
+            GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+    {
+        currentCoordX = csbi.dwCursorPosition.X;
+        currentCoordY = csbi.dwCursorPosition.Y;
+    }
+    else{
+        currentCoordX = 0;
+        currentCoordY = 0;
+    }
+}
+
+void moveToCurrentCoord(){
+    destCoord.X = currentCoordX;
+    destCoord.Y = currentCoordY;
+    SetConsoleCursorPosition(hStdout, destCoord);
+}
+
+void removePreviousLine(){
+    saveCurrentCoord();
+    cout << "                                                                  ";
+    moveToCurrentCoord();
+}
+
+void moveToStartCoord(){
+    destCoord.X = 0;
+    destCoord.Y = 1;
+    SetConsoleCursorPosition(hStdout, destCoord);
+}
+
+void test1(){
+    moveToStartCoord();
+    for (int i = 0; i < 11; i++) {
+        for (int j = 0; j < 11; j++) {
+            if(i == 5 && j == 5) {
+                cout << 1 << " ";
+            }else{
+                cout << 0 << " ";
+            }
+        }
+        cout << endl;
+    }
+    moveToMessageCoord();
+}
+
+void test2(){
+    moveToStartCoord();
+    for (int i = 0; i < 11; i++) {
+        for (int j = 0; j < 11; j++) {
+            if(i == 5 && j == 5) {
+                cout << 0 << " ";
+            }else{
+                cout << 0 << " ";
+            }
+        }
+        cout << endl;
+    }
+    moveToMessageCoord();
+}
+
 void ConsoleDisplay::drawField(Field field) {
+    moveToStartCoord();
     cout << "Game field:" << endl;
     for (int i = 0; i < field.getSize(); i++) {
         for (int j = 0; j < field.getSize(); j++) {
@@ -21,17 +99,27 @@ void ConsoleDisplay::drawField(Field field) {
         }
         cout << endl;
     }
+    moveToMessageCoord();
 }
 
 void ConsoleDisplay::showWrongMove() {
+    removePreviousLine();
     cout << "Wrong input!" << endl;
 }
 
 void ConsoleDisplay::showWinner(int type) {
-
+    removePreviousLine();
+    if(type == 0){
+        cout << "First player win";
+    } else if (type == 1){
+        cout << "Second player win";
+    } else {
+        cout << "Draw";
+    }
 }
 
 Position ConsoleDisplay::getFirstPlayerMove() {
+    removePreviousLine();
     cout << "<Player1>enter position: ";
     int x, y;
     cin >> x >> y;
@@ -39,8 +127,11 @@ Position ConsoleDisplay::getFirstPlayerMove() {
 }
 
 Position ConsoleDisplay::getSecondPlayerMove() {
+    removePreviousLine();
     cout << "<Player2>enter position: ";
     int x, y;
     cin >> x >> y;
     return Position(x, y);
 }
+
+
