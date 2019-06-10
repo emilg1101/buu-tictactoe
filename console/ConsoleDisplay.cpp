2f86c652f27ff149956a6131c1a038889d8ff56b
Nazar <nazarfatichov@mail.ru>
2019-06-10 10:49:00 +0300

    Fix simple version

--- console/ConsoleDisplay.cpp
+++ console/ConsoleDisplay.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 #include <windows.h>
 #include "ConsoleDisplay.h"
 
@@ -14,7 +15,11 @@ struct Coord{
     }
 };
 
-const Coord START_COORD = Coord(0,0);
+string firstPlayerName;
+string secondPlayerName;
+
+bool isMultiPlayer;
+
 const Coord START_TABLE_COORD = Coord(0, 1);
 const Coord START_PLAYER_INPUT_COORD = Coord(0, 13);
 const Coord START_MESSAGE_COORD = Coord(0, 12);
@@ -25,22 +30,17 @@ CONSOLE_SCREEN_BUFFER_INFO csbi;
 HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
 COORD destCoord;
 
-ConsoleDisplay::ConsoleDisplay() {}
-
-ConsoleDisplay::~ConsoleDisplay() {}
-
-Configuration ConsoleDisplay::getConfiguration() {
-
-    Configuration configuration = Configuration();
-    configuration.IS_MULTIPLAYER = true;
-    return configuration;
-}
-
 int getAvailableColumns(){
     CONSOLE_SCREEN_BUFFER_INFO sbInfo;
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &sbInfo);
     int availableColumns = sbInfo.dwSize.X;
     return availableColumns;
+}
+
+void moveToSecondLineStartCoord(){
+    destCoord.X = 0;
+    destCoord.Y = 1;
+    SetConsoleCursorPosition(hStdout, destCoord);
 }
 
 void moveToTableStartCoord(){
@@ -88,8 +88,8 @@ void removeLineFromCurrentCoord(){
 }
 
 void moveToStartCoord() {
-    destCoord.X = START_COORD.x;
-    destCoord.Y = START_COORD.y;
+    destCoord.X = 0;
+    destCoord.Y = 0;
     SetConsoleCursorPosition(hStdout, destCoord);
 }
 
@@ -98,18 +98,47 @@ void removeMessage(){
     removeLineFromCurrentCoord();
 }
 
-void ConsoleDisplay::drawField(Field field) {
+void askIsMultiPlayer(){
+    string mp;
+    moveToSecondLineStartCoord();
+    removeLineFromCurrentCoord();
+    cout << "Is Multi Player Game[Y/N]: ";
+    cin >> mp;
+    if (mp.compare("Y") == 0 ){
+        isMultiPlayer = true;
+    } else if(mp.compare("N") == 0 ){
+        isMultiPlayer = false;
+    } else {
+        moveToStartCoord();
+        cout << "Write only \"Y\" or \"N\" please";
+        askIsMultiPlayer();
+    }
+    moveToStartCoord();
+    removeLineFromCurrentCoord();
+    moveToSecondLineStartCoord();
+    removeLineFromCurrentCoord();
+    moveToStartCoord();
+}
+
+void askOneUserName(){
+    cout << "Write player name: ";
+    cin >> firstPlayerName;
+    secondPlayerName = "Computer";
     moveToStartCoord();
     removeLineFromCurrentCoord();
     moveToStartCoord();
-    cout << "Game field:" << endl;
-    moveToTableStartCoord();
-    for (int i = 0; i < field.getSize(); i++) {
-        for (int j = 0; j < field.getSize(); j++) {
-            cout << field[j][i] << " ";
-        }
-        cout << endl;
-    }
+}
+
+void askTwoUsersName(){
+    cout << "Write 1 player name: ";
+    cin >> firstPlayerName;
+    cout << "Write 2 player name: ";
+    cin >> secondPlayerName;
+    moveToStartCoord();
+    removeLineFromCurrentCoord();
+    moveToSecondLineStartCoord();
+    removeLineFromCurrentCoord();
+    moveToStartCoord();
 }
 
 void ConsoleDisplay::showWrongMove() {
@@ -132,11 +161,41 @@ void ConsoleDisplay::showWinner(int type) {
     }
 }
 
+ConsoleDisplay::ConsoleDisplay() {}
+
+ConsoleDisplay::~ConsoleDisplay() {}
+
+Configuration ConsoleDisplay::getConfiguration() {
+    askIsMultiPlayer();
+    if(isMultiPlayer){
+        askTwoUsersName();
+    } else {
+        askOneUserName();
+    }
+    Configuration configuration = Configuration();
+    configuration.IS_MULTIPLAYER = true;
+    return configuration;
+}
+
+void ConsoleDisplay::drawField(Field field) {
+    moveToStartCoord();
+    removeLineFromCurrentCoord();
+    moveToStartCoord();
+    cout << "Game field:" << endl;
+    moveToTableStartCoord();
+    for (int i = 0; i < field.getSize(); i++) {
+        for (int j = 0; j < field.getSize(); j++) {
+            cout << field[j][i] << " ";
+        }
+        cout << endl;
+    }
+}
+
 Position ConsoleDisplay::getFirstPlayerMove() {
     moveToPlayerInputStartCoord();
     removeLineFromCurrentCoord();
     moveToPlayerInputStartCoord();
-    cout << "<Player1>enter position: ";
+    cout << "<" << firstPlayerName << ">enter position: ";
     int x, y;
     cin >> x >> y;
     removeMessage();
@@ -147,7 +206,7 @@ Position ConsoleDisplay::getSecondPlayerMove() {
     moveToPlayerInputStartCoord();
     removeLineFromCurrentCoord();
     moveToPlayerInputStartCoord();
-    cout << "<Player2>enter position: ";
+    cout << "<" << secondPlayerName << ">enter position: ";
     int x, y;
     cin >> x >> y;
     removeMessage();
