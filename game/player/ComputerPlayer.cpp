cc2f4d1979782f116d662764c0dc9ee31555f8ce
emilg1101 <emilyandro99@gmail.com>
2019-06-12 21:34:54 +0800

    Computer player

--- game/player/ComputerPlayer.cpp
+++ game/player/ComputerPlayer.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "ComputerPlayer.h"
 
 struct Move {
@@ -10,6 +11,39 @@ ComputerPlayer::ComputerPlayer() {
     field = Field(11);
 }
 
+
+int checkY(Position pos, int type, int dir) {
+    Position newPos = Position(pos.x, pos.y+dir);
+    if (!field.inRange(newPos)) return 0;
+    if (field.get(newPos) != type) return 0;
+    return 1 + checkY(newPos, type, dir);
+}
+
+int checkX(Position pos, int type, int dir) {
+    Position newPos = Position(pos.x+dir, pos.y);
+    if (!field.inRange(newPos)) return 0;
+    if (field.get(newPos) != type) return 0;
+    return 1 + checkX(newPos, type, dir);
+}
+
+int checkXY(Position pos, int type, int xdir, int ydir) {
+    Position newPos = Position(pos.x + xdir, pos.y + ydir);
+    if (!field.inRange(newPos)) return 0;
+    if (field.get(newPos) != type) return 0;
+    return 1 + checkXY(newPos, type, xdir, ydir);
+}
+
+bool checkWin(int type, Position pos) {
+
+    int vert = checkY(pos, type, -1) + checkY(pos, type, 1) + 1;
+    int hor = checkX(pos, type, -1) + checkX(pos, type, 1) + 1;
+    int lr = checkXY(pos, type, 1, 1) + checkXY(pos, type, -1, -1) + 1;
+    int rl = checkXY(pos, type, -1, 1) + checkXY(pos, type, 1, -1) + 1;
+
+    //cout << "checkWin" << endl;
+    return vert == WIN_COUNT || hor == WIN_COUNT || lr == WIN_COUNT || rl == WIN_COUNT;
+}
+
 bool isMovesLeft() {
     for (int i = 0; i < field.getSize(); i++)
         for (int j = 0; j < field.getSize(); j++)
@@ -19,10 +53,29 @@ bool isMovesLeft() {
 }
 
 int evaluate() {
-    return 10;
+    for (int i = 0; i < field.getSize(); i++) {
+        for (int j = 0; j < field.getSize(); j++) {
+            if (checkWin(CIRCLE_CELL_CODE, Position(i, j))) {
+                return  10;
+            }
+        }
+    }
+
+    for (int i = 0; i < field.getSize(); i++) {
+        for (int j = 0; j < field.getSize(); j++) {
+            if (checkWin(CROSS_CELL_CODE, Position(i, j))) {
+                return  -10;
+            }
+        }
+    }
+
+    return 0;
 }
 
 int minimax(int depth, bool isMax) {
+    cout << depth << endl;
+    if (depth == 1)
+        return 0;
     int score = evaluate();
 
     if (score == 10)
