16dd6ea0cf8afb7a1482adc3025900884ff7905d
emilg1101 <emilyandro99@gmail.com>
2019-06-16 10:36:22 +0800

    Added Saver interface

---
+++ game/storage/Saver.cpp
@@ -0,0 +1,17 @@
+#include "Saver.h"
+
+Saver::Saver() {
+
+}
+
+bool Saver::hasSavedGame() {
+    return false;
+}
+
+Save Saver::getSave() {
+    return Save(Field(), 0);
+}
+
+void Saver::newSave(Field field, int lastMovePlayer) {
+
+}
--- main.cpp
+++ main.cpp
@@ -4,9 +4,18 @@
 #include "game/player/FirstPlayer.h"
 #include "game/player/SecondPlayer.h"
 #include "game/player/ComputerPlayer.h"
+#include "game/storage/Saver.h"
 
 int main() {
+    Saver saver = Saver();
     Display* display = new ConsoleDisplay();
+
+    if (saver.hasSavedGame()) {
+        cout << "Has game" << endl;
+    } else {
+        cout << "No game" << endl;
+    }
+
     Configuration configuration = display->getConfiguration();
 
     PlayerIOStream* firstPlayer = new FirstPlayer(display);
